0c4a585c5159b2630f6520daedc374b1112fa41e
Rehambadr <61743765+Rehambadr@users.noreply.github.com>
2020-03-31 17:33:25 +0200

    Add files via upload

---
+++ WebSite3/dbo.Table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE NewStudent
+(
+	[Name : ] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [ID : ] NCHAR(10) NOT NULL, 
+    [E.mail : ] NVARCHAR(50) NOT NULL, 
+    [Password : ] NVARCHAR(50) NOT NULL, 
+    [Gender : ] NVARCHAR(50) NOT NULL, 
+    [Nationality : ] NVARCHAR(50) NOT NULL, 
+    [Address : ] NVARCHAR(MAX) NOT NULL, 
+    [City : ] NVARCHAR(50) NOT NULL, 
+    [Phone number : ] NVARCHAR(50) NOT NULL, 
+    [Faculty : ] NVARCHAR(50) NOT NULL, 
+    [Bus :] NVARCHAR(50) NOT NULL
+)
